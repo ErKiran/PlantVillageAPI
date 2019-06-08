e612105045b56d9fb58212bc8922d9e259fe65d1
Kiran Adhikari <kiruu1238@gmail.com>
2019-06-08 10:40:53 +0545

    API DOne

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const data = require('../../data/xlsxtojson.json');
+const datatoinsert = require('../../data/xlsxtojson.json');
 
 const Unst = require('../../models/unstructured');
 
-router.get('/submit', () => {
-    const db = new Unst({ data });
-    db.save()
+router.get('/submit', async (req, res) => {
+    const data = await Unst.find({});
+    if (data.length === 0) {
+        Unst.insertMany(datatoinsert)
+        res.json('Data sucessfully added')
+    }
+    else {
+        res.json(data)
+    }
+});
+
+router.get('/desc/:disease', async (req, res) => {
+    const getdes = await Unst.findOne({ DISEASE: req.params.disease });
+    if (!getdes) {
+        res.json(`We are unable to find ${req.params.disease} in our database`);
+    }
+    else {
+        res.json(getdes)
+    }
 })
+
 
 
 module.exports = router
